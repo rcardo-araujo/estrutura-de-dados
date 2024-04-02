9aba4c92cb7d82522d229932f5023c933e0d73da
Ricardo Araujo <ricardoasf@id.uff.br>
2024-04-02 18:16:30 -0300

    árvore binária de busca

---
+++ bibliotecas/arvore-binaria-busca/TABB.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "TABB.h"
+
+TABB* TABB_inicializa(void) {
+    return NULL;
+}
+
+TABB* TABB_cria(int info, TABB* esq, TABB* dir) {
+    TABB* novo = (TABB *)malloc(sizeof(TABB));
+    novo->info = info;
+    novo->esq = esq;
+    novo->dir = dir;
+
+    return novo;
+}
+
+TABB* TABB_insere(TABB* arv, int x) {
+    if(!arv) return TABB_cria(x, NULL, NULL);
+
+    if(x < arv->info) arv->esq = TABB_insere(arv->esq, x);
+    else if(x > arv->info) arv->dir = TABB_insere(arv->dir, x);
+
+    return arv;
+}
+
+TABB* TABB_retira(TABB* arv, int x) {
+    if(!arv) return arv;
+
+    if(x != arv->info) {
+        if(x < arv->info) arv->esq = TABB_retira(arv->esq, x);
+        else arv->dir = TABB_retira(arv->dir, x);
+    } else {
+        if(TABB_folha(arv)) {
+            free(arv);
+            return NULL;
+        } else {
+            if(arv->esq && arv->dir) {
+                TABB* p = arv->esq;
+                while(p->dir) p = p->dir;
+                arv->info = p->info;
+                p->info = x;
+                arv->esq = TABB_retira(arv->esq, x); 
+            } else {
+                TABB* temp = arv;
+                if(arv->esq) arv = arv->esq;
+                else arv = arv->dir;
+                free(temp);
+            }
+        }
+    }
+    
+    return arv;
+}
+
+TABB* TABB_busca(TABB* arv, int x) {
+    if(!arv || (arv->info == x)) return arv;
+
+    if(x < arv->info) return TABB_busca(arv->esq, x);
+    return TABB_busca(arv->dir, x);
+}
+
+void TABB_imprime(TABB* arv, int tab) {
+    if(arv) {
+        int t;
+        for(t = 0; t < tab; t ++)
+            printf("-");
+        
+        printf(" %d", arv->info);
+        TABB_imprime(arv->esq, tab + 2);
+        TABB_imprime(arv->dir, tab + 2);
+    }
+}
+
+int TABB_folha(TABB* no) {
+    return !(no->esq || no->dir);
+}
---
+++ bibliotecas/arvore-binaria-busca/TABB.h
@@ -0,0 +1,18 @@
+#ifndef TABB_H
+#define TABB_H
+
+typedef struct ABB {
+    int info;
+    struct ABB* esq;
+    struct ABB* dir;
+} TABB;
+
+TABB* TABB_inicializa(void);
+TABB* TABB_cria(int info, TABB* esq, TABB* dir);
+TABB* TABB_insere(TABB* arv, int x);
+TABB* TABB_retira(TABB* arv, int x);
+TABB* TABB_busca(TABB* arv, int x);
+void TABB_imprime(TABB* arv, int tab);
+int TABB_folha(TABB* no);
+
+#endif
